96ab2653d67b2f34a043259b350e8a99345e3785
workmakerdaily <sver12@naver.com>
2025-03-18 20:34:17 +0900

    25.03.18 20:34

--- amuz_to_do.sql
+++ amuz_to_do.sql
@@ -1,81 +1,28 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: amuz
--- ------------------------------------------------------
--- Server version	8.0.40
+CREATE DATABASE amuz;
+USE amuz;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE users (
+  user_id varchar(20) NOT NULL,
+  user_name varchar(20) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (user_id)
+);
 
---
--- Table structure for table `calendar_data`
---
+CREATE TABLE to_do_lists (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id varchar(20) NOT NULL,
+  goal text NOT NULL,
+  is_checked tinyint NOT NULL DEFAULT '0',
+  priority int NOT NULL DEFAULT '0',
+  checked_date varchar(10) DEFAULT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT to_do_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
 
-DROP TABLE IF EXISTS `calendar_data`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `calendar_data` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(20) NOT NULL,
-  `goal` text NOT NULL,
-  `completed_date` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `to_do_lists`
---
-
-DROP TABLE IF EXISTS `to_do_lists`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `to_do_lists` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(20) NOT NULL,
-  `goal` text NOT NULL,
-  `is_checked` tinyint NOT NULL DEFAULT '0',
-  `priority` int NOT NULL DEFAULT '0',
-  `checked_date` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `to_do_user_id_idx` (`user_id`),
-  CONSTRAINT `to_do_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `user_id` varchar(20) NOT NULL,
-  `user_name` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `userId_UNIQUE` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-03-18 13:51:48
+CREATE TABLE calendar_data (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id varchar(20) NOT NULL,
+  goal text NOT NULL,
+  completed_date varchar(10) NOT NULL,
+  PRIMARY KEY (id)
+);
